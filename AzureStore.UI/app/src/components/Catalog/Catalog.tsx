90861e8c2113bd19bc096ee5c11dad4918206033
Harbaruk <grishagarbaruk1995@gmail.com>
2020-10-19 20:48:20 +0300

    feature: add filtering header; add endpoint to filter data by price

--- AzureStore.UI/app/src/components/Catalog/Catalog.tsx
+++ AzureStore.UI/app/src/components/Catalog/Catalog.tsx
@@ -1,30 +1,45 @@
 import React from 'react';
 import { Container, Grid } from '@material-ui/core';
+import HeaderFilter from '../HeaderFilter';
 import CatalogItem from '../Shared/CatalogItem';
+import { SimpleSearchFilter } from '../../models/SimpleSearchFilter';
 
 interface Props {
     getCatalog: () => void;
+    filterCatalog: (price: SimpleSearchFilter) => void;
     catalogItems: Array<any>;
 }
 
 export default class Catalog extends React.Component<Props> {
 
+    state = {
+        maxPrice: 0
+    };
+
     componentDidMount() {
         this.props.getCatalog();
     }
 
-    showFullDescription = (id: number) => {
-        alert(id);
+    componentDidUpdate() {
+        const items = this.props.catalogItems;
+        if (items.length > 0 && this.state.maxPrice === 0) {
+            const max = Math.max.apply(Math, items.map(it => it.price));
+            this.setState({ maxPrice: max });
+        }
     }
 
     render() {
-        const data = this.props.catalogItems.map(item => {
+        const data = this.props.catalogItems ? this.props.catalogItems.map(item => {
             return <Grid item sm={6} md={4} lg={3}>
                 <CatalogItem item={item}></CatalogItem>
             </Grid>
-        });
+        }) : [];
+
         return (
             <Container>
+                <HeaderFilter
+                    maxPrice={this.state.maxPrice}
+                    filterProducts={this.props.filterCatalog}></HeaderFilter>
                 <Grid container
                     spacing={3}
                     direction="row"
--- AzureStore.UI/app/src/containers/CatalogContainer.tsx
+++ AzureStore.UI/app/src/containers/CatalogContainer.tsx
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
-import { getCatalogItems } from '../actions/catalog'
+import { filterCatalogItems, getCatalogItems } from '../actions/catalog'
 import Catalog from '../components/Catalog';
+import { SimpleSearchFilter } from '../models/SimpleSearchFilter';
 
 const mapStateToProps = (state: any) => {
     return {
@@ -10,7 +11,8 @@ const mapStateToProps = (state: any) => {
 
 const mapDispatchToProps = (dispatch: any) => {
     return {
-        getCatalog: () => dispatch(getCatalogItems())
+        getCatalog: () => dispatch(getCatalogItems()),
+        filterCatalog: (filter: SimpleSearchFilter) => dispatch(filterCatalogItems(filter))
     };
 }
 
