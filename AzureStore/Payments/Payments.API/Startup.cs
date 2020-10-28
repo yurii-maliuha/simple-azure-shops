ba1569f6a1bfa02504329781990622c76b7d105e
Harbaruk <grishagarbaruk1995@gmail.com>
2020-10-28 21:09:21 +0200

    feature/payments: add empty API project

---
+++ AzureStore/Payments/Payments.API/Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Payments.API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment)
+        {
+            _configuration = configuration;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (_hostingEnvironment.IsDevelopment())
+            {
+                services.AddCors(o => o.AddPolicy("CORS", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                }));
+            }
+            services.AddSwaggerGen(c =>
+            {
+                c.DescribeAllParametersInCamelCase();
+                c.SwaggerDoc("v1", new OpenApiInfo());
+            });
+            services.AddLogging();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseCors("CORS");
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(x => x.MapControllers());
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog V1");
+            });
+        }
+    }
+}
