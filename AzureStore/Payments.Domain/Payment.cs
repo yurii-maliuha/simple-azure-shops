34ac241d1b2f7c49672319cf1c7051d177f416d5
Harbaruk <grishagarbaruk1995@gmail.com>
2020-10-29 19:45:36 +0200

    feat/payments: base infrastructure

---
+++ AzureStore/Payments.Domain/Payment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payments.Domain
+{
+    public class Payment
+    {
+        public Guid Id { get; set; }
+        public string Status { get; set; }
+        public DateTimeOffset? Created { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+    }
+}
