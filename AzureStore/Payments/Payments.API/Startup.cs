bbb714cb9a8f810949ca93aca758f5d8c22bf433
Harbaruk <grishagarbaruk1995@gmail.com>
2020-11-07 19:21:03 +0200

    feat/payments: get all payments and post endpoints; add admin policy;

--- AzureStore/Payments/Payments.API/Startup.cs
+++ AzureStore/Payments/Payments.API/Startup.cs
@@ -1,12 +1,21 @@
+using System;
+using System.Text;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Payments.API.Policy;
 using Payments.Persistence;
 using Payments.Persistence.Repositories;
+using Payments.Services.Handlers;
+using Payments.Services.Mappers;
 
 namespace Payments.API
 {
@@ -49,6 +58,28 @@ namespace Payments.API
             });
             services.AddLogging();
             services.AddControllers();
+
+            services.AddAutoMapper(typeof(PaymentsMapper));
+
+            services.AddMediatR(typeof(GetAllPaymentsHandler));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ClockSkew = TimeSpan.Zero,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        //ValidIssuer = Configuration.GetSection("Jwt")["ValidIssuer"],
+                        //ValidAudience = Configuration.GetSection("Jwt")["ValidAudience"],
+                        //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetSection("Jwt")["Key"]))
+                    };
+                });
+
+            services.AddAuthorization(options => options.AddPolicy(Policies.Admin, Admin.Policy));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,8 +92,10 @@ namespace Payments.API
             }
 
             app.UseRouting();
+            app.UseAuthorization();
             app.UseEndpoints(x => x.MapControllers());
             app.UseSwagger();
+
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog V1");
---
+++ AzureStore/Payments/Payments.Services/Commands/GetAllPaymentsCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+using Payments.Services.Models;
+
+namespace Payments.Services.Commands
+{
+    public class GetAllPaymentsCommand : IRequest<IEnumerable<PaymentInfo>>
+    {
+    }
+}
